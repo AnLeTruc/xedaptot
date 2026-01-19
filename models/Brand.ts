f0446c4e415a084185e7c0fce9c8c1ff9e7d9e36
LeTrucAn <letrucan096666@gmail.com>
2026-01-19 19:24:54 +0700

    feat(models)models: add Brand model

---
+++ models/Brand.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+import { IBrandDocument } from '../types/index';
+
+const brandSchema = new Schema<IBrandDocument>(
+    {
+        name: {
+            type: String,
+            required: [true, 'Brand name is required'],
+            unique: true,
+            trim: true
+        },
+        country: {
+            type: String,
+            trim: true
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        }
+    }, { timestamps: true }
+);
+
+//Index
+brandSchema.index({ name: 1 });
+brandSchema.index({ isActive: 1 });
+
+const Brand = mongoose.model<IBrandDocument>('Brand', brandSchema);
+
+export default Brand;
