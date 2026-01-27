c117929399911e09f59ab2d0d5611d31919c6f05
LeTrucAn <letrucan096666@gmail.com>
2026-01-27 18:09:30 +0700

    fix: fix conflict main

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,12 +1,39 @@
 import { Router } from 'express';
-import { firebaseAuth, getProfile, updateProfile, addAddress, updateAddress, deleteAddress, setDefaultAddress } from '../controllers/authController';
+import { 
+    firebaseAuth, 
+    getProfile, 
+    updateProfile, 
+    addAddress, 
+    updateAddress, 
+    deleteAddress, 
+    setDefaultAddress,
+    emailRegister,
+    emailLogin,
+    refreshToken,
+    sendEmailVerification,
+    verifyEmail,
+    forgotPassword,
+    verifyResetCode,
+    resetPassword
+    } from '../controllers/authController';
 import { verifyToken, requireUser } from '../middleware/auth';
 
 const router = Router();
 
-//Firebase Authentication
+// Firebase auth 
 router.post('/firebase', firebaseAuth);
-router.post('')
+router.post('/email/register', emailRegister);
+router.post('/email/login', emailLogin);
+router.post('/refresh-token', refreshToken);
+
+//Mail verify
+router.post('/send-verification-email', verifyToken, requireUser, sendEmailVerification);
+router.get('/verify-email', verifyEmail);
+
+//Reset - change password
+router.post('/forgot-password', forgotPassword);
+router.post('/verify-reset-code', verifyResetCode);
+router.post('/reset-password', resetPassword);
 
 //Profile
 router.get('/profile', verifyToken, requireUser, getProfile);
