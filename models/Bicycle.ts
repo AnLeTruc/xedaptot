b6484ea2c3786cd7520c0195e5ecf91ca0276aad
LeTrucAn <letrucan096666@gmail.com>
2026-03-12 19:03:01 +0700

    hotfix: fix coordinates, address

--- models/Bicycle.ts
+++ models/Bicycle.ts
@@ -154,7 +154,7 @@ const bicycleSchema = new Schema<IBicycleDocument>(
 
 bicycleSchema.index({ title: 'text', description: 'text' });
 
-bicycleSchema.index({ 'location.coordinates': '2dsphere' });
+bicycleSchema.index({ 'location.coordinates': '2dsphere' }, { sparse: true });
 
 
 const Bicycle = mongoose.model<IBicycleDocument>('Bicycle', bicycleSchema);
