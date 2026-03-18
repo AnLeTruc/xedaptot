0bef97afd7ee209c3fbc1928b2a118d778cbfcbf
LeTrucAn <letrucan096666@gmail.com>
2026-03-19 01:31:29 +0700

    hotfix: fix field sellerEmail by avatarUrl

--- types/summary.ts
+++ types/summary.ts
@@ -61,7 +61,7 @@ export interface TopCategoriesResult {
 export interface SellerChartItem {
   sellerId: string;
   sellerName: string;
-  avatarUrl: string;
+  sellerEmail: string;
   successOrders: number;
   percentage: number;
 }
